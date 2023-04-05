73bb3e807ffe906c006efa240dc241ee6ed48568
ramon <ramon@petize.com.br>
2023-04-04 22:03:51 -0300

    Iniciando projeto receitas

---
+++ src/pages/datail/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Datail() {
+  return (
+    <View style={styles.container}>
+      <Text>Página datelhes da receita...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'blue'
+  },
+});
---
+++ src/pages/search/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Search() {
+  return (
+    <View style={styles.container}>
+      <Text>Página Search(Buscar)...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'green'
+  },
+});
